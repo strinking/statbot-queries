8a19c682cb88a47be8e2fe149151b0fa97727007
Ammon Smith <ammon.i.smith@gmail.com>
2017-11-29 22:55:22 -0800

    Add mentions query.

---
+++ mention-counts.sql
@@ -0,0 +1,20 @@
+--- Displays which users have been mentioned the most
+--- Invalid mentions are ignored
+
+SELECT
+    mentions.user_id,
+    users.name,
+    mentions.count AS mentions
+FROM (
+    SELECT
+        mentioned_id AS user_id,
+        COUNT(mentioned_id)
+    FROM mentions
+    WHERE guild_id = 181866934353133570 -- Programming
+    GROUP BY user_id
+) AS mentions
+JOIN users
+    ON users.user_id = mentions.user_id
+GROUP BY mentions.user_id, mentions.count, users.name
+ORDER BY mentions DESC;
+
