3658f20d6ac36bf1eaccf5e5b7ef7e98b4b37988
Ammon Smith <ammon.i.smith@gmail.com>
2018-03-12 15:45:40 -0700

    Update embeds query.

--- selfbot_embeds.sql
+++ selfbot_embeds.sql
@@ -1,11 +1,13 @@
--- This query counts how many messages have embeds,
+-- This query enumerates users who have embeds,
 -- but were not made by bot accounts.
--- This means that they are running selfbots.
+-- This might mean that they are running selfbots.
 
-SELECT count(*)
+SELECT DISTINCT users.user_id, users.name, count(messages.message_id)
 FROM messages
 JOIN users
     ON messages.user_id = users.user_id
 WHERE messages.guild_id = 181866934353133570 -- Programming
 AND messages.embeds != '[]'
-AND NOT users.is_bot;
+AND NOT users.is_bot
+GROUP BY users.user_id, users.name
+ORDER BY count DESC;
