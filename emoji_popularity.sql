ac32e0c6050b84461f35c010a0bcc721bce3c3e9
Ammon Smith <ammon.i.smith@gmail.com>
2018-03-12 17:36:53 -0700

    Update emoji query.

--- emoji_popularity.sql
+++ emoji_popularity.sql
@@ -3,16 +3,15 @@
 
 SELECT
     ss.emoji[2] AS name,
-    ss.emoji[3] AS id,
     ss.emoji[1] = 'a' AS animated,
-    count(ss.emoji[3])
+    count(ss.emoji)
 FROM (
     SELECT (
-        SELECT regexp_matches(content, '<(a?):([A-Za-z0-9_-]+):([0-9]+)>')
+        SELECT regexp_matches(content, '<(a?):([A-Za-z0-9_-]+):[0-9]+>', 'i')
     ) AS emoji
     FROM messages
     WHERE guild_id = 181866934353133570 -- Programming
 ) AS ss
 WHERE ss.emoji IS NOT NULL
-GROUP BY ss.emoji
-ORDER BY count DESC;
+GROUP BY ss.emoji[2], ss.emoji[1]
+ORDER BY count DESC, name ASC;
