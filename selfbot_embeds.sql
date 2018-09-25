7ddb1ed62f064f9cafc9c498209f3f4181131ee9
Ammon Smith <ammon.i.smith@gmail.com>
2018-09-25 15:49:12 -0400

    Convert possible selfbot embed query.

--- selfbot_embeds.sql
+++ selfbot_embeds.sql
@@ -1,13 +1,15 @@
 -- This query enumerates users who have embeds,
 -- but were not made by bot accounts.
 -- This might mean that they are running selfbots.
+-- However, do know that embeds are also generated normally
+-- by Discord for attached files or links.
 
-SELECT DISTINCT users.user_id, users.name, count(messages.message_id)
+SELECT DISTINCT users.name, users.real_user_id, users.int_user_id, COUNT(messages.message_id)
 FROM messages
 JOIN users
-    ON messages.user_id = users.user_id
+    ON messages.int_user_id = users.int_user_id
 WHERE messages.guild_id = 181866934353133570 -- Programming
 AND messages.embeds != '[]'
 AND NOT users.is_bot
-GROUP BY users.user_id, users.name
+GROUP BY users.name, users.real_user_id, users.int_user_id
 ORDER BY count DESC;
