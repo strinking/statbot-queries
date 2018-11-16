3e51c16a7985c50c998fac396759728505dc67b2
Ammon Smith <ammon.i.smith@gmail.com>
2018-11-16 13:16:43 -0500

    Reformat selfbot_embeds.sql

--- selfbot_embeds.sql
+++ selfbot_embeds.sql
@@ -4,12 +4,16 @@
 -- However, do know that embeds are also generated normally
 -- by Discord for attached files or links.
 
-SELECT DISTINCT users.name, users.real_user_id, users.int_user_id, COUNT(messages.message_id)
+SELECT DISTINCT
+    users.name,
+    users.real_user_id,
+    users.int_user_id,
+    COUNT(messages.message_id)
 FROM messages
 JOIN users
-    ON messages.int_user_id = users.int_user_id
+    ON users.int_user_id = messages.int_user_id
 WHERE messages.guild_id = 181866934353133570 -- Programming
-AND messages.embeds != '[]'
-AND NOT users.is_bot
+    AND messages.embeds != '[]'
+    AND NOT users.is_bot
 GROUP BY users.name, users.real_user_id, users.int_user_id
 ORDER BY count DESC;
