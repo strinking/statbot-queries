56a0d1ab4cab6b3094a98a1265bdf9f51cd0fcac
Ammon Smith <ammon.i.smith@gmail.com>
2019-06-22 19:01:40 -0400

    Add snowflake_time() SQL function.

---
+++ id_to_snowflake.sql
@@ -0,0 +1,11 @@
+-- Helper function to convert Discord IDs into snowflakes purely within Postgres
+-- Equivalent in Python:
+--  datetime.utcfromtimestamp(((id >> 22) + DISCORD_EPOCH) / 1000)
+-- where DISCORD_EPOCH is 1420070400000
+
+CREATE FUNCTION snowflake_time(id bigint)
+    RETURNS TIMESTAMP WITH TIME ZONE
+    LANGUAGE SQL
+    AS 'SELECT to_timestamp(((id >> 22) + 1420070400000) / 1000) AS timestamp'
+    RETURNS NULL ON NULL INPUT
+    IMMUTABLE;
